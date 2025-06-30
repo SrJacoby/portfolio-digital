f1cbd9b4a80d76557358f31e6fcbbb85abd6089a
SrJacoby <luizjacoby294@gmail.com>
2025-06-30 16:26:44 -0300

    finalizando página de projetos

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
 import LinkedInLogo from '../assets/linkedin.svg'
 import GitHubLogo from '../assets/github.svg'
-import './Footer.module.css'
+import styles from'./Footer.module.css'
 
 const Footer = () => {
   return (
     <footer>
-        <ul>
+        <ul className={styles.listaFooter}>
             <li><Link to="https://github.com/SrJacoby" target="_blank"><span><img src={GitHubLogo} alt="Github logo" />GitHub</span></Link></li>
             <li><Link to="https://www.linkedin.com/in/luiz-jacoby/" target="_blank"><span><img src={LinkedInLogo} alt="LinkedIn logo" />LinkedIn</span></Link></li>
         </ul>
